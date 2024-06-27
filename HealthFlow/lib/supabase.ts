836b1a8162464ca7c2985c7d961b3d229fa86e9f
Liilice <alice.zheng@epitech.eu>
2024-06-27 16:29:24 +0200

    initial structure

---
+++ HealthFlow/lib/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tvudmxokkdxjcxoeaveh.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2dWRteG9ra2R4amN4b2VhdmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NzUzNTIsImV4cCI6MjAzNTA1MTM1Mn0.L1Dn1_XCb7Cro3BHYVsqsHjxvxiGBQqSHnliyaGj7Xo";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
